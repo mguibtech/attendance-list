36e6b1c3ef06ec590ca575e5923a619243b352e0
Marcos <marcos.guibson@precojusto.app.br>
2023-02-18 16:12:04 -0400

    Creatting form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,32 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
 
 export function Home() {
+
+
+
+  function handleParticipandAdd() {
+    console.log("Foi clicado no botão")
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Name of particpant"
+          placeholderTextColor='#6b6b6b'
+          keyboardType="default"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipandAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -14,5 +14,33 @@ export const styles = StyleSheet.create({
   eventDate:{
     fontSize: 14,
     color:'#6b6b6b'
+  },
+  input:{
+    height: 56,
+    backgroundColor:'#1f1e25',
+    borderRadius: 5,
+    color: '#fdfcfe',
+    padding: 16,
+    fontSize:16,
+    flex: 1,
+    marginRight: 12
+  },
+  buttonText:{
+    color: '#fff'
+  },
+  button:{
+    width: 56,
+    height: 56,
+    borderRadius: 6,
+    backgroundColor: '#31cf67',
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  form:{
+    width: '100%',
+    flexDirection: "row",
+    marginTop: 36,
+    marginBottom: 42
   }
 })
