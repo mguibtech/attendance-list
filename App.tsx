6a4a88c665fd8d1b1b5b1f32ad6e1cb32b494171
Marcos <marcos.guibson@precojusto.app.br>
2023-02-18 15:24:36 -0400

    Creatting page structure of Home

--- App.tsx
+++ App.tsx
@@ -1,29 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Home } from "./src/screens/Home";
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
-      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
-    </View>
+    <Home/>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    backgroundColor: '#131016'
-  },
-  eventName: {
-    fontSize: 18,
-    color:'#fff',
-    fontWeight: "bold"
-  },
-  eventDate:{
-    fontSize: 14,
-    color:'#6b6b6b'
-  }
-})
 
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#131016'
+  },
+  eventName: {
+    fontSize: 18,
+    color:'#fff',
+    fontWeight: "bold"
+  },
+  eventDate:{
+    fontSize: 14,
+    color:'#6b6b6b'
+  }
+})
